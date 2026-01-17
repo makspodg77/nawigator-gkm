62768f2b5f72099e5d9c4cd50547b11d89d05d7d
Maksymilian Podgórski <mp224ps@student.lnu.se>
2026-01-17 02:29:54 +0100

    init

--- jak-doris-dojedzie/src/App.tsx
+++ jak-doris-dojedzie/src/App.tsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+function MapClickHandler({
+  onLocationClick,
+}: {
+  onLocationClick: (lat: number, lon: number) => void;
+}) {
+  useMapEvents({
+    click: (e) => {
+      const { lat, lng } = e.latlng;
+      onLocationClick(lat, lng);
+    },
+  });
+
+  return null;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [routes, setRoutes] = useState([]);
+  const [route, setRoute] = useState([]);
+  const [geometryToMap, setGeometryToMap] = useState();
+  const [originCoords, setOriginCoords] = useState();
+  const [destinationCoords, setDestinationCoords] = useState();
+
+  useEffect(() => {
+    const getRoute = async () => {
+      const response = await fetch("http://localhost:2137/csa-route", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          lat1: 14.773679894104593,
+          lon1: 53.46201924878469,
+          lat2: 14.818750752236157,
+          lon2: 53.564039679676135,
+          startTime: 500,
+          endTime: 620,
+        }),
+      });
+
+      const data = await response.json();
+      console.log(data);
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MapContainer
+        center={[53.56380862022675, 14.828390433293993]}
+        zoom={13}
+        style={{ height: "100vh", width: "100%", display: "block" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <MapClickHandler
+          onLocationClick={(lat, lon) => console.log(lat, lon)}
+        />
+      </MapContainer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
